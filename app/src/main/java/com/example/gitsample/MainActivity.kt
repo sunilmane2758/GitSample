a8fec5ea760eaa5597323261ad410f22ea1f9004
Admin <sunil.mane@risertechub.in>
2023-10-12 16:22:16 +0530

    Modified Mainactivity.kt file

--- app/src/main/java/com/example/gitsample/MainActivity.kt
+++ app/src/main/java/com/example/gitsample/MainActivity.kt
@@ -11,5 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* Example of Git Romote Url
+        git remote add origin https://github.com/sunilmane2758/GitSample.git
+       */
     }
 }
